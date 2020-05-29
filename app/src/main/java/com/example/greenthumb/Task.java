1f8c5c945b32be733bcdf16fb9122bc70a528a77
Ben Kelly <ben.kelly@dal.ca>
2020-05-28 22:13:07 -0300

    Created Task class

--- app/src/main/java/com/example/greenthumb/Task.java
+++ app/src/main/java/com/example/greenthumb/Task.java
@@ -1,4 +1,15 @@
 package com.example.greenthumb;
 
+import java.util.Date;
+
 public class Task {
+    private TaskType type;
+    private Date dueDate;
+    private User owner;
+
+    public Task(TaskType type, Date dueDate, User owner) {
+        this.type = type;
+        this.dueDate = dueDate;
+        this.owner = owner;
+    }
 }
