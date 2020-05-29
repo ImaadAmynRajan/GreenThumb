7070069b9a1edb887828238e10f69f6b88f1f939
Ben Kelly <ben.kelly@dal.ca>
2020-05-28 22:11:33 -0300

    Added task titles and task type enum

---
+++ app/src/main/java/com/example/greenthumb/Task.java
@@ -0,0 +1,4 @@
+package com.example.greenthumb;
+
+public class Task {
+}
---
+++ app/src/main/java/com/example/greenthumb/TaskType.java
@@ -0,0 +1,12 @@
+package com.example.greenthumb;
+
+public enum TaskType {
+    SEASONAL,
+    FERTILIZE,
+    WEED,
+    TRIM,
+    EQUIPMENT,
+    HEALTH,
+    PEST,
+    CLEAN
+}
