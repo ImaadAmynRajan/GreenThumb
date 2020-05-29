0dfa0fe3c4e8b0c8342ddc1b7cc06198e93c9638
Ben Kelly <ben.kelly@dal.ca>
2020-05-29 10:30:18 -0300

    Fixed unknown identifier error in ViewTasks.java

--- app/src/main/java/com/example/greenthumb/ViewTasks.java
+++ app/src/main/java/com/example/greenthumb/ViewTasks.java
@@ -16,8 +16,6 @@ public class ViewTasks extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tasks);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.addTaskButton);
         fab.setOnClickListener(new View.OnClickListener() {
