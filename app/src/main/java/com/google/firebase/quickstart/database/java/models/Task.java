bba4e2f7b48050ce50130eede6977d9fcd98b19b
Courtney Pick <courtneypick@gmail.com>
2020-06-06 12:52:33 -0300

    Changed userId to user and made dueDate of type Date rather than String.

--- app/src/main/java/com/google/firebase/quickstart/database/java/models/Task.java
+++ app/src/main/java/com/google/firebase/quickstart/database/java/models/Task.java
@@ -2,18 +2,20 @@ package com.google.firebase.quickstart.database.java.models;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.util.Date;
+
 @IgnoreExtraProperties
 public class Task {
     public int taskId;
     public String taskType;
-    public String dueDate;
-    public User userId;
+    public Date dueDate;
+    public User user;
     public Task(){}
 
-    public Task(int taskId, String taskType, String dueDate, User userId){
+    public Task(int taskId, String taskType, Date dueDate, User user){
         this.taskId = taskId;
         this.taskType = taskType;
         this.dueDate = dueDate;
-        this.userId = userId;
+        this.user = user;
     }
 }
