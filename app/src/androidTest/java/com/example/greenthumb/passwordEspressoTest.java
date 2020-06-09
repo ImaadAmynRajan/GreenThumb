3ec226a94deea4c49f47327908cd7f90610cb740
rajan <https://git.cs.dal.ca/rajan/csci3130.git>
2020-06-09 16:11:14 -0300

    Junit test and espresso test

---
+++ app/src/androidTest/java/com/example/greenthumb/passwordEspressoTest.java
@@ -0,0 +1,49 @@
+package com.example.greenthumb;
+
+import android.view.KeyEvent;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class passwordEspressoTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+
+    @Test
+    public void testStrongPassword(){
+        onView(withId(R.id.editTextTextPassword))
+                .perform(click())
+                .perform(typeText("Nihir@123"));
+        onView(withId(R.id.signUp_button))
+                .perform(click());
+        onView(withId(R.id.textView))
+                .check(matches(withText("Strong password")));
+    }
+
+
+    @Test
+    public void testWeakPassword(){
+        onView(withId(R.id.editTextTextPassword))
+                .perform(click())
+                .perform(typeText("nihir@123"));
+        onView(withId(R.id.signUp_button))
+                .perform(click());
+        onView(withId(R.id.textView))
+                .check(matches(withText("Weak password, enter a stronger one.")));
+    }
+}
