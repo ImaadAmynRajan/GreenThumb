7338cd24c5443ea91a2ff0d3d771f7f27234841d
Janessa Ridley <jn358585@dal.ca>
2020-06-18 19:30:45 -0300

    Espresso tests added for home page and navigation UI

---
+++ app/src/androidTest/java/com/example/greenthumb/HomePageEspressoTest.java
@@ -0,0 +1,35 @@
+package com.example.greenthumb;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+
+public class HomePageEspressoTest {
+
+    @Rule
+    public ActivityScenarioRule<HomePageActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(HomePageActivity.class);
+
+    @Test
+    public void verifyNavigationDisplayed() {
+        onView(withId(R.id.mainNav))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.toHomePage))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.toTaskPage))
+                .check(matches(isDisplayed()));
+    }
+
+}
