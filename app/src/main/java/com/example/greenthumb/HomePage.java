0fae4ccaa60a7f0186264efdbdc3bdc1dac69ea4
usman sheikh <us349705@dal.ca>
2020-06-30 14:57:54 -0300

    1. added code to logout a user from home page. espresso test to check logout functionality passes

--- app/src/main/java/com/example/greenthumb/HomePage.java
+++ app/src/main/java/com/example/greenthumb/HomePage.java
@@ -1,8 +1,16 @@
 package com.example.greenthumb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomePage extends NavigationBar {
+    Button logout_button;
+    FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
 
 
     @Override
@@ -10,6 +18,17 @@ public class HomePage extends NavigationBar {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         super.onCreateNav();
+        logout_button = findViewById(R.id.logout_button);
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent loginPage = new Intent(HomePage.this, MainActivity.class);
+                startActivity(loginPage);
+
+            }
+        });
+
     }
 
 }
