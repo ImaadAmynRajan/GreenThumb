1f42facc192793a68b822d141fa6847c950a1eeb
Ben Kelly <ben.kelly@dal.ca>
2020-07-12 21:02:38 -0300

    Added TaskTitle enum

---
+++ app/src/main/java/com/example/greenthumb/TaskTitle.java
@@ -0,0 +1,16 @@
+package com.example.greenthumb;
+
+import androidx.databinding.InverseMethod;
+
+/**
+ * Enum representing the different types of gardening tasks.
+ * Code based on Province.java from Assignment 4.
+ */
+public enum TaskTitle {
+    None, PlantSeasonal, Fertilize, Weed, TrimBushes, MaintainEquipment, MonitorHealth, RemovePests, ClearDebris;
+
+    @InverseMethod("intToTitle")
+    public static int titleToInt (TaskTitle title) { return (title == null) ? -1 : title.ordinal(); }
+    public static TaskTitle intToTitle (int titleIdx) { return (titleIdx == -1) ? null : TaskTitle.values()[titleIdx]; }
+
+}
