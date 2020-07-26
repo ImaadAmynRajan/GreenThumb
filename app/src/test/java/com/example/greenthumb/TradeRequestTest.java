d6648354093a3d61df845792e6a66578bb78af05
usman sheikh <us349705@dal.ca>
2020-07-25 21:17:13 -0300

    Junit Tests for Trade Request class

---
+++ app/src/test/java/com/example/greenthumb/TradeRequestTest.java
@@ -0,0 +1,25 @@
+package com.example.greenthumb;
+
+import com.example.greenthumb.tasks.Task;
+import com.example.greenthumb.trade.TradeRequest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TradeRequestTest {
+    private User user;
+
+    @Test
+    public void getTradeRequest() {
+        TradeRequest tradeRequest = new TradeRequest("userid", new User("userid", "email@email.com"), new Task());
+        // assert the tradeRequest doesn't have an assignee yet
+        assertTrue(tradeRequest.getAssigneeId(), true);
+//        assertTrue(tradeRequest.getRequestedTask(),true);
+        // assign a user
+        tradeRequest.setRequestedTradeUser(new User("user-2-id", "email@email.com"));
+        // check that the values are updated
+        assertEquals("email@email.com", tradeRequest.getRequestedTradeUser().getEmail());
+        assertEquals("userid", tradeRequest.getAssigneeId());
+    }
+}
